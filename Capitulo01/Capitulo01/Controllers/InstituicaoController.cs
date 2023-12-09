a258424722d70977ff8960078f7a1138aac3e7bb
Raphaela Ferraz <raphaelagferraz2@gmail.com>
2023-12-09 20:07:34 -0300

    feat: criação da controlador da Instituicao

---
+++ Capitulo01/Capitulo01/Controllers/InstituicaoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Capitulo01.Controllers
+{
+    public class InstituicaoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("oi");
+        }
+    }
+}
