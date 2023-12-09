2156818f8ab9de13dad9b2abb3745f97cae14e3b
Raphaela Ferraz <raphaelagferraz2@gmail.com>
2023-12-09 20:36:09 -0300

    feat: criação da view para o modelo Instituição

--- Capitulo01/Capitulo01/Controllers/InstituicaoController.cs
+++ Capitulo01/Capitulo01/Controllers/InstituicaoController.cs
@@ -1,12 +1,45 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Capitulo01.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Capitulo01.Controllers
 {
     public class InstituicaoController : Controller
     {
+        private static IList<Instituicao> instituicoes = new List<Instituicao>()
+        {
+            new Instituicao() {
+                InsituticaoID = 1,
+                Nome = "UniParaná",
+                Endereco = "Paraná"
+            },
+            new Instituicao() {
+                InsituticaoID = 2,
+                Nome = "UniSanta",
+                Endereco = "Santa Catarina"
+            },
+            new Instituicao()
+            {
+                InsituticaoID = 3,
+                Nome = "UniSãoPaulo",
+                Endereco = "São Paulo"
+            },
+            new Instituicao()
+            {
+                InsituticaoID = 4,
+                Nome = "UniSulGrandense",
+                Endereco = "Rio Grande do Sul"
+            },
+            new Instituicao()
+            {
+                InsituticaoID = 5,
+                Nome = "UniCarioca",
+                Endereco = "Rio de Janeiro"
+            }
+        };
+
         public IActionResult Index()
         {
-            return View("oi");
+            return View(instituicoes);
         }
     }
 }
--- Capitulo01/Capitulo01/Models/Instituicao.cs
+++ Capitulo01/Capitulo01/Models/Instituicao.cs
@@ -2,5 +2,8 @@
 {
     public class Instituicao
     {
+        public long? InsituticaoID { get; set; }
+        public string Nome { get; set;}
+        public string Endereco { get; set;}
     }
 }
