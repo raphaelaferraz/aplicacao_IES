2bea05f5e3b396a8a8e26df921b8bce3dcbc01ea
Raphaela Ferraz <raphaelagferraz2@gmail.com>
2024-01-03 16:48:19 -0300

    feat: criação do contexto de banco de dados

--- Capitulo01/Capitulo01/Data/IESContext.cs
+++ Capitulo01/Capitulo01/Data/IESContext.cs
@@ -1,8 +1,16 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Capitulo01.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Capitulo01.Data
 {
     public class IESContext : DbContext
     {
+        public IESContext(DbContextOptions<IESContext> options) : base(options) 
+        { 
+            
+        }
+
+        public DbSet<Departamento> Departamentos { get; set;}
+        public DbSet<Instituicao> Instituicoes { get; set;}
     }
 }
