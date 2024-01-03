685cb6ecc1ca1d6a1b7b8838b4177b682b41116a
Raphaela Ferraz <raphaelagferraz2@gmail.com>
2024-01-03 17:17:36 -0300

    feat: classe para inserir dados quando o modelo é inicializado

---
+++ Capitulo01/Capitulo01/Data/IESDbInitializer.cs
@@ -0,0 +1,32 @@
+﻿using Capitulo01.Models;
+
+namespace Capitulo01.Data
+{
+    public class IESDbInitializer
+    {
+        public static void Initialize(IESContext context)
+        {
+            // A base de dados é criada quando o método abaixo é invocado
+            context.Database.EnsureCreated();
+
+            // Verifica se há algum objeto/registro em Departamentos usando LINQ
+            if(context.Departamentos.Any()) 
+            {
+                return;
+            }
+
+            var departamentos = new Departamento[]
+            {
+                new Departamento {Nome = "Ciência da Computação"},
+                new Departamento {Nome = "Ciência de Alimentos"}
+            };
+
+            foreach(Departamento departamento in departamentos)
+            {
+                context.Departamentos.Add(departamento);
+            }
+
+            context.SaveChanges(); 
+        }
+    }
+}
