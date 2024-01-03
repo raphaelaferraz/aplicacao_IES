aa2d5a2a2419c3731f2b006d0d9dd6ff4cab20b6
Raphaela Ferraz <raphaelagferraz2@gmail.com>
2024-01-03 17:36:48 -0300

    feat: criação do controller Departamento

---
+++ Capitulo01/Capitulo01/Controllers/DepartamentoController.cs
@@ -0,0 +1,21 @@
+﻿using Capitulo01.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Capitulo01.Controllers
+{
+    public class DepartamentoController : Controller
+    {
+        private readonly IESContext _context;
+
+        public DepartamentoController(IESContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Departamentos.OrderBy(c => c.Nome).ToListAsync());
+        }
+    }
+}
