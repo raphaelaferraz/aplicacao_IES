d789add10bb6aa3d41d2f5e52c67a572dc5d4d18
Raphaela Ferraz <raphaelagferraz2@gmail.com>
2024-01-18 12:06:21 -0300

    feat: criação dos métodos de create do departamento

--- Capitulo01/Capitulo01/Controllers/DepartamentoController.cs
+++ Capitulo01/Capitulo01/Controllers/DepartamentoController.cs
@@ -1,4 +1,5 @@
 ﻿using Capitulo01.Data;
+using Capitulo01.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -17,6 +18,33 @@ namespace Capitulo01.Controllers
         public async Task<IActionResult> Index()
         {
             return View(await _context.Departamentos.OrderBy(c => c.Nome).ToListAsync());
+        }
+
+        //GET: Departamento/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Nome")]Departamento departamento)
+        {
+            try
+            {
+                if(ModelState.IsValid)
+                {
+                    _context.Add(departamento);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+            catch (DbUpdateException)
+            {
+                ModelState.AddModelError("", "Não foi possível inserir os dados.");
+            }
+
+            return View(departamento);
         }
     }
 }
